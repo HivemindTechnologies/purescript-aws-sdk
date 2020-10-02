7050bb6e9d8cebbd59f99503344b51c493483d92
realvictorprm <mueller.vpr@gmail.com>
2020-10-02 11:51:59 +0200

    Reformat & add LogGroupName newtype

--- src/AWS/CloudWatchLogs/CloudwatchLogs.purs
+++ src/AWS/CloudWatchLogs/CloudwatchLogs.purs
@@ -1,7 +1,6 @@
 module CloudWatchLogs where
 
 import Prelude
-
 import AWS.Core (AccessKeyId(..), Region(..), SecretAccessKey(..), SessionToken(..))
 import Control.Promise (Promise)
 import Control.Promise as Promise
@@ -15,6 +14,9 @@ import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
 
 foreign import data API :: Type
+
+newtype LogGroupName
+  = LogGroupName String
 
 type InternalMakeClientParams
   = { region :: String
@@ -45,44 +47,42 @@ type InternalDescribeLogGroupsResponse
 type DescribeLogGroupsResponse
   = { logGroups :: Array LogGroup }
 
-type InternalLogGroup 
-  = 
-          { arn :: Nullable String
-          , creationTime :: Nullable Number
-          , kmsKeyId ::  Nullable String
-          , logGroupName :: Nullable String
-          , metricFilterCount :: Nullable Number
-          , retentionInDays :: Nullable Number
-          , storedBytes :: Nullable Number 
-          }
+type InternalLogGroup
+  = { arn :: Nullable String
+    , creationTime :: Nullable Number
+    , kmsKeyId :: Nullable String
+    , logGroupName :: Nullable String
+    , metricFilterCount :: Nullable Number
+    , retentionInDays :: Nullable Number
+    , storedBytes :: Nullable Number
+    }
 
 -- todo: Number to TimeStamp
-type LogGroup 
-  = 
-          { creationTime :: Maybe Number
-          , logGroupName :: Maybe String
-          , retentionInDays :: Maybe Number
-          , storedBytes :: Maybe Number 
-          }
+type LogGroup
+  = { creationTime :: Maybe Number
+    , logGroupName :: Maybe String
+    , retentionInDays :: Maybe Number
+    , storedBytes :: Maybe Number
+    }
 
 type InternalDescribeLogStreamsResponse
   = { logStreams :: Array InternalLogStream }
 
-type InternalLogStream 
- = { creationTime :: Nullable Number
- , firstEventTimestamp:: Nullable Number
- , lastEventTimestamp:: Nullable Number
- , lastIngestionTime:: Nullable Number
- }
+type InternalLogStream
+  = { creationTime :: Nullable Number
+    , firstEventTimestamp :: Nullable Number
+    , lastEventTimestamp :: Nullable Number
+    , lastIngestionTime :: Nullable Number
+    }
 
 -- todo: Number to TimeStamp
-type LogStream 
- = { creationTime :: Maybe Number
- , firstEventTimestamp:: Maybe Number
- , lastEventTimestamp:: Maybe Number
- , lastIngestionTime:: Maybe Number
+type LogStream
+  = { creationTime :: Maybe Number
+    , firstEventTimestamp :: Maybe Number
+    , lastEventTimestamp :: Maybe Number
+    , lastIngestionTime :: Maybe Number
+    }
 
- }
 type DescribeLogStreamsResponse
   = { logStreams :: Array LogStream }
 
@@ -92,34 +92,34 @@ describeLogGroups :: API -> Aff DescribeLogGroupsResponse
 describeLogGroups api = liftEffect curried >>= Promise.toAff <#> toExternal
   where
   toExternal :: InternalDescribeLogGroupsResponse -> DescribeLogGroupsResponse
-  toExternal {logGroups: internal} = {logGroups : internal <#> toExternalLogGroup} 
+  toExternal { logGroups: internal } = { logGroups: internal <#> toExternalLogGroup }
 
   toExternalLogGroup :: InternalLogGroup -> LogGroup
-  toExternalLogGroup internal = 
-      { creationTime : Nullable.toMaybe internal.creationTime
-      , logGroupName : Nullable.toMaybe internal.logGroupName
-      , retentionInDays : Nullable.toMaybe internal.retentionInDays
-      , storedBytes : Nullable.toMaybe internal.storedBytes 
-      } 
+  toExternalLogGroup internal =
+    { creationTime: Nullable.toMaybe internal.creationTime
+    , logGroupName: Nullable.toMaybe internal.logGroupName
+    , retentionInDays: Nullable.toMaybe internal.retentionInDays
+    , storedBytes: Nullable.toMaybe internal.storedBytes
+    }
 
   curried :: Effect (Promise InternalDescribeLogGroupsResponse)
   curried = runFn1 describeLogGroupsImpl api
 
 foreign import describeLogStreamsImpl :: Fn2 API String (Effect (Promise InternalDescribeLogStreamsResponse))
 
-describeLogStreams :: API -> String -> Aff (DescribeLogStreamsResponse)
-describeLogStreams api name = liftEffect (curried api name) >>= Promise.toAff <#> toExternal
+describeLogStreams :: API -> LogGroupName -> Aff (DescribeLogStreamsResponse)
+describeLogStreams api (LogGroupName name) = liftEffect (curried api name) >>= Promise.toAff <#> toExternal
   where
   toExternal :: InternalDescribeLogStreamsResponse -> DescribeLogStreamsResponse
-  toExternal {logStreams: internal} = {logStreams : internal <#> toExternalLogStream} 
+  toExternal { logStreams: internal } = { logStreams: internal <#> toExternalLogStream }
 
   toExternalLogStream :: InternalLogStream -> LogStream
-  toExternalLogStream internal = 
-      { creationTime : Nullable.toMaybe internal.creationTime
-      , firstEventTimestamp : Nullable.toMaybe internal.firstEventTimestamp
-      , lastEventTimestamp : Nullable.toMaybe internal.lastEventTimestamp
-      , lastIngestionTime : Nullable.toMaybe internal.lastIngestionTime 
-      } 
+  toExternalLogStream internal =
+    { creationTime: Nullable.toMaybe internal.creationTime
+    , firstEventTimestamp: Nullable.toMaybe internal.firstEventTimestamp
+    , lastEventTimestamp: Nullable.toMaybe internal.lastEventTimestamp
+    , lastIngestionTime: Nullable.toMaybe internal.lastIngestionTime
+    }
 
   curried :: API -> String -> Effect (Promise InternalDescribeLogStreamsResponse)
-  curried  = (runFn2 describeLogStreamsImpl) 
+  curried = (runFn2 describeLogStreamsImpl)
