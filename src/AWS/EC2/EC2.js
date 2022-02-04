3b50b798cef36d21ee269f1625079d872707c5f2
tiphanie <tiphanie.dousset@hivemindtechnologies.com>
2022-02-03 18:43:35 -0600

    Revert "Revert "Add filters to describeInstances function""
    This reverts commit 8cd3d6ea4392f8e5cb898483a46d4518e20d3021.

--- src/AWS/EC2/EC2.js
+++ src/AWS/EC2/EC2.js
@@ -5,8 +5,8 @@ const EC2 = require('aws-sdk/clients/ec2')
 exports.newEC2 = (params) =>
   () => new EC2(params)
 
-exports.describeInstancesImpl = (ec2) =>
-  () => ec2.describeInstances().promise()
+exports.describeInstancesImpl = (ec2, filters) =>
+  () => ec2.describeInstances({ Filters: filters }).promise()
 
 exports.describeTagsImpl = (ec2, filters) =>
   () => ec2.describeTags({ Filters: filters }).promise()
